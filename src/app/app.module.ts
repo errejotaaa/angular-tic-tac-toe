71a643f3fbe4cbe1f42f5f47e7a80dd491b3cffa
errejotaaa <ricardojaa.91@gmail.com>
2021-05-12 17:37:14 -0400

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './square/square.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SquareComponent, BoardComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css'],
+})
+export class BoardComponent implements OnInit {
+  squares: string[];
+  private isXmark: boolean = false;
+  winner: string;
+  noWinner: boolean = false;
+
+  get player() {
+    return this.isXmark ? 'O' : 'X';
+  }
+  constructor() {}
+
+  ngOnInit(): void {
+    this.onNewGame();
+  }
+
+  onNewGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.isXmark = false;
+    this.noWinner = false;
+  }
+
+  onClickSquare(index: number) {
+    if (!this.squares[index] && !this.winner) {
+      if (this.isXmark) {
+        this.squares[index] = 'O';
+      } else {
+        this.squares[index] = 'X';
+      }
+      this.isXmark = !this.isXmark;
+    }
+    this.winner = this.isWinner();
+    this.noWinner = this.isNoWinner();
+  }
+
+  isWinner() {
+    let lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      let [a, b, c] = lines[i];
+
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+
+  isNoWinner() {
+    let count = 0;
+    for (let i = 0; i < this.squares.length; i++) {
+      if (this.squares[i] !== null) {
+        count++;
+      }
+    }
+    if (count === this.squares.length) {
+      return true;
+    }
+
+    return false;
+  }
+}
